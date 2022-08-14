7c8a9c3d34248ddc227328db7326c1583b64f9ba
Brique Au Bob <briqueaubob@gmail.com>
2022-08-15 01:02:58 +0200

    fix url

--- composables/Axios.ts
+++ composables/Axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: 'https://api.umaestro.fr/'
-    baseURL: 'http://localhost:3333/'
+    baseURL: 'https://api.umaestro.fr/'
+    // baseURL: 'http://localhost:3333/'
 })
