834c91429aef9e215f727201056f5877d0066b19
Brique au bob <36899582+BriqueAuBob@users.noreply.github.com>
2022-11-12 09:11:13 +0100

    bot multid token update

--- server/api/message.ts
+++ server/api/message.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default defineEventHandler(async () => {
-    const bot_token = 'NzgxOTAxMDQ0OTQxNTg2NDMy.X8EYGA.sa2mE3c76YzlgG_RUgx9C8j9ZlA';
+    const bot_token = 'OTgzMDk0NTI4NzkxNjgzMTgy.Gx_WPk.s6jYU2iQZmVE_zsblrNOc1mknpLVXj9N0QEpZM';
 
     const { data } = await axios.get('https://discord.com/api/v8/channels/802261784110694470/messages/971179872389054514', {
         headers: {
