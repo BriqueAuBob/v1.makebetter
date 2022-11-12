432223ab4310a1e2644b88eb41e092fc3835daca
Brique au bob <36899582+BriqueAuBob@users.noreply.github.com>
2022-11-12 09:19:45 +0100

    Update message.ts

--- server/api/message.ts
+++ server/api/message.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
     const bot_token = 'OTgzMDk0NTI4NzkxNjgzMTgy.Gx_WPk.s6jYU2iQZmVE_zsblrNOc1mknpLVXj9N0QEpZM';
 const url = event.context.params.url
     const { data } = await axios.get(url, {
