42c5310d58b99d4f9a9ba4cf237d78c7e90aaba9
Brique au bob <36899582+BriqueAuBob@users.noreply.github.com>
2022-11-12 09:33:09 +0100

    Update message.ts

--- server/api/message.ts
+++ server/api/message.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export default defineEventHandler(async (event) => {
     const bot_token = 'OTgzMDk0NTI4NzkxNjgzMTgy.Gx_WPk.s6jYU2iQZmVE_zsblrNOc1mknpLVXj9N0QEpZM';
-const url = event.context.params.url
+const query = getQuery(event)
+const url = query.url
     const { data } = await axios.get(url, {
         headers: {
             Authorization: 'Bot ' + bot_token
