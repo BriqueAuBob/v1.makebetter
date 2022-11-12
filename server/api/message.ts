7a1c26339b22ed3f892dc12ac5b6d763e18a4cd7
Brique au bob <36899582+BriqueAuBob@users.noreply.github.com>
2022-11-12 09:51:48 +0100

    Update message.ts

--- server/api/message.ts
+++ server/api/message.ts
@@ -4,11 +4,13 @@ export default defineEventHandler(async (event) => {
     const bot_token = 'OTgzMDk0NTI4NzkxNjgzMTgy.Gx_WPk.s6jYU2iQZmVE_zsblrNOc1mknpLVXj9N0QEpZM';
 const query = getQuery(event)
 const url = query.url
-    const { data } = await axios.get(url, {
+const elements = url.split("/")
+const urlApi = ˋhttps://discord.com/api/v9/channels/${elements[5]}/messages/${elements[6]}`
+    const { data } = await axios.get(urlApi, {
         headers: {
             Authorization: 'Bot ' + bot_token,
 "Content-Type": "application/json"
         }
     });
-    return {data};
+    return {...data, ...data.author};
 })
